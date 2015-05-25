d09b6f63cb10aa1df5ae461c6fc09bc49f8f228f
Sebastian Dirrigl <sebastian.dirrigl@nttdata.com>
2015-05-25 18:04:09 +0200

    added webjars and bootstrap support

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import models.*;
 import play.*;
+import play.data.*;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +10,6 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
-
 }
---
+++ app/models/User.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by DIRRIS on 25.05.2015.
+ */
+public class User {
+}
