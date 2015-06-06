86e428efa7b7112569d2e44c1b8a9b71fdd5272f
Aaron Perez Martin <aapemar@inf.upv.es>
2015-06-06 18:13:35 +0200

    Add login file

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,4 +12,7 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render());
     }
+    //public static Result login() { return ok(login.render("Yeahh")); }
+    public static Result login() { return ok(views.html.login.render("asd")); }
+
 }
