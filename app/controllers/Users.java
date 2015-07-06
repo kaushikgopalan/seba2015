0364bc9027340dc00b0398349f441e6dc33a2462
Aaron Perez Martin <aapemar@inf.upv.es>
2015-07-06 19:44:55 +0200

    System suggests all other helpies with sames skills according with the user skills

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -321,9 +321,9 @@ public class Users extends Controller{
 
         List<Category> mySkills = user.skills;
         List<User> helpiesSuggestion = new ArrayList<>();
-        List<User> lastUsers = User.getLastHelps();
+        List<User> lastUsers = User.find.all();
 
-        System.out.println("After Funtion.My skills are: " + mySkills.size());
+        //System.out.println("After Funtion.My skills are: " + mySkills.size());
 
         //nothing in DB
         if(mySkills.size()==0 || lastUsers.size()==0){
@@ -345,8 +345,8 @@ public class Users extends Controller{
                         if (skillsSuggestion.get(z).name == mySkills.get(j).name) {
                             helpiesSuggestion.add(num, lastUsers.get(i));
                             num++;
-                            System.out.println("Users according my skills: " + skillsSuggestion.get(z).name);
-                            System.out.println("My skills are: " + mySkills.size());
+                            //System.out.println("Users according my skills: " + skillsSuggestion.get(z).name);
+                            //System.out.println("My skills are: " + mySkills.size());
                             if(num == 5){
                                 return helpiesSuggestion;
                             }
